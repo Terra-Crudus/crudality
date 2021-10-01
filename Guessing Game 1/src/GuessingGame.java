924faa4db499997c6d5b3ca2c8a06f4368d576c2
Lederman <S01916398>
2021-10-01 09:06:11 -0600

    Editing and fixed all of guessing game 1 and 2

--- Guessing Game 1/src/GuessingGame.java
+++ Guessing Game 1/src/GuessingGame.java
@@ -1,9 +1,9 @@
 /*  Emily Lederman
  * Guessing Game 1
  * Due: 09/30/21
- * Description: Generating a random number between 10 - 20 and user will try to guess it, code will output both numbers
- * 
- * 
+ * Description: Generating random number between 10-20, 
+ * user will try to guess it, 
+ * code will output both numbers
  */
 import java.util.Scanner;
 import java.util.Random;
@@ -11,34 +11,30 @@ public class GuessingGame {
 	static Scanner input; 
 	public static void main(String[] args) {
 		input = new Scanner(System.in);
-		int guess; //The users guess//
-			
-		Random randGen; //random number generator//
+		int guess; //The users guess
+		Random randGen; //random number generator
 
-			//getting the users guess//
+			//getting the users guess
 		System.out.println("What number would you like to guess? I will generate one between ten and twenty. ");
 		guess = input.nextInt();
-		
 		System.out.println("Your guess is " + guess);
-	
 		randGen = new Random();
-		int answer; /* int answer, number created that person guesses for*/
+		int answer; // int answer, number created that person guesses for
 			//formula is (high - low + 1) + low
 		answer = randGen.nextInt(20 - 10 + 1) + 10;
 		System.out.println("The random number generated is " + answer);
 		System.out.println("Your guess was " + guess);
+		if (guess > answer)
+		{
+			System.out.println("The guess is too high.");
+		}
+		else
+		{
+			System.out.println("The guess is too low.");
+		}
 	}
 	
 }	
 	
-	
-/*Notes begin here
- *  System.out.println allows you to print out a string
- *  System.in
- *  input
- *  java.util
- *  randGen and New Random 
- *  git pull (once you cd into the correct folder), git add ., git commit -m, git push
- */
-
-// Problems and headbangers were: forgot to add Random randGen, continued to need space before end quotes so output looked correct//
+/* Problems and headbangers were: forgot to add Random randGen, 
+continued to need space before end quotes so output looked correct*/
